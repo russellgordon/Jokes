8be1b49cdb88ac77f6072eb4be6f35fd314851d1
Russell Gordon <rgordon@lcs.on.ca>
2023-04-14 08:19:18 -0400

    Re-organized to add View group.

---
+++ Jokes/Views/JokeView.swift
@@ -0,0 +1,20 @@
+//
+//  JokeView.swift
+//  Jokes
+//
+//  Created by Russell Gordon on 2023-04-14.
+//
+
+import SwiftUI
+
+struct JokeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct JokeView_Previews: PreviewProvider {
+    static var previews: some View {
+        JokeView()
+    }
+}
