afabdbade584472cd0b2339ecda75876cd697a5f
Russell Gordon <rgordon@lcs.on.ca>
2023-04-14 08:27:35 -0400

    Added joke setup and nav title.

--- Jokes/Views/JokeView.swift
+++ Jokes/Views/JokeView.swift
@@ -9,7 +9,14 @@ import SwiftUI
 
 struct JokeView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack {
+                Text("You see, mountains aren't just funny.")
+                    .font(.title)
+                    .multilineTextAlignment(.center)
+            }
+            .navigationTitle("Random Jokes")
+        }
     }
 }
 
