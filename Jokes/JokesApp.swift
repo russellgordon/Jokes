6a0c71b12a06c416c89e5959838db4a1f5a0262e
Russell Gordon <rgordon@lcs.on.ca>
2023-04-14 07:59:44 -0400

    Initial Commit

---
+++ Jokes/JokesApp.swift
@@ -0,0 +1,17 @@
+//
+//  JokesApp.swift
+//  Jokes
+//
+//  Created by Russell Gordon on 2023-04-14.
+//
+
+import SwiftUI
+
+@main
+struct JokesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
